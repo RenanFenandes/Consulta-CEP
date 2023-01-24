92f33da6645b33468160fd22b3c0259d7b011193
Renan Fernandes <Renanribeiro1540@hotmail.com>
2023-01-24 17:38:20 -0300

    first commit

---
+++ js/consulta-endereco.js
@@ -0,0 +1,30 @@
+let cep = document.querySelector('#cep');
+let rua = document.querySelector('#rua');
+let bairro = document.querySelector('#bairro');
+let cidade = document.querySelector('#cidade');
+let estado = document.querySelector('#estado');
+let ddd = document.querySelector('#ddd')
+
+cep.value = '';
+
+cep.addEventListener('blur', function(e) {
+    let cep = e.target.value;
+    let script = document.createElement('script');
+    script.src = 'https://viacep.com.br/ws/'+cep+'/json/?callback=popularForm';
+    document.body.appendChild(script);
+});
+
+function popularForm(resposta) {
+
+    if("erro" in resposta) {
+        alert('CEP não encontrado');
+        return;
+    }
+
+    console.log(resposta);
+    rua.value = resposta.logradouro;
+    bairro.value = resposta.bairro;
+    cidade.value = resposta.localidade;
+    estado.value = resposta.uf;
+    ddd.value = resposta.ddd
+}
---
+++ js/consulta-endereço.js
@@ -0,0 +1,30 @@
+let cep = document.querySelector('#cep');
+let rua = document.querySelector('#rua');
+let bairro = document.querySelector('#bairro');
+let cidade = document.querySelector('#cidade');
+let estado = document.querySelector('#estado');
+let numero = document.querySelector('#ddd')
+
+cep.value = '01001000';
+
+cep.addEventListener('blur', function(e) {
+    let cep = e.target.value;
+    let script = document.createElement('script');
+    script.src = 'https://viacep.com.br/ws/'+cep+'/json/?callback=popularForm';
+    document.body.appendChild(script);
+});
+
+function popularForm(resposta) {
+
+    if("erro" in resposta) {
+        alert('CEP não encontrado');
+        return;
+    }
+
+    console.log(resposta);
+    rua.value = resposta.logradouro;
+    bairro.value = resposta.bairro;
+    cidade.value = resposta.localidade;
+    estado.value = resposta.uf;
+    numero.value = resposta.ddd
+}
